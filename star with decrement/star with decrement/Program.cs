1d262c8fa8995f591b8ec7744515552eb684b059
Your Name <you@example.com>
2020-12-21 20:48:51 +0500

    first commit

---
+++ star with decrement/star with decrement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace star_with_decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 8; row >= 1; --row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ star/star/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 8; ++row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
